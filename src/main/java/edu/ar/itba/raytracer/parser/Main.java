7b8626c1d2bdc176582e2f567fc127484b7247f9
Matias Domingues <mannias@gmail.com>
2015-05-25 09:06:10 -0300

    other merge

--- src/main/java/edu/ar/itba/raytracer/parser/Main.java
+++ src/main/java/edu/ar/itba/raytracer/parser/Main.java
@@ -8,17 +8,8 @@ import java.io.IOException;
  * Created by matiasdomingues on 5/24/15.
  */
 public class Main {
-<<<<<<< HEAD
     public static void main(String[] argv) throws IOException {
         FileInput input = new FileInput(new File("charizard/untitled.lxs"));
         input.parse();
-=======
-	public static void main(String[] argv) throws IOException {
-		for (int i = 0; i < 5; i++) {
-			FileInput input = new FileInput(new File("charizard\\untitled.lxs"));
-			input.parse();
-		}
->>>>>>> f73c8c778030bba078776d189d1d88109362da95
-
 	}
 }
