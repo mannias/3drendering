8285787f90b929b3260c69fa1c55a438950c74e9
Matias Domingues <mannias@gmail.com>
2015-06-28 04:02:07 -0300

    adding cosine distribution function

--- src/main/java/edu/ar/itba/raytracer/samplers/Sampler.java
+++ src/main/java/edu/ar/itba/raytracer/samplers/Sampler.java
@@ -73,4 +73,32 @@ public class Sampler {
         Collections.shuffle(sampleList);
     }
 
+    public static Vector4 cosWeightedHemisphere( Vector4 normal )
+    {
+        double Xi1 = Math.random();
+        double Xi2 = Math.random();
+        double  theta = Math.acos(Math.sqrt(1.0-Xi1));
+        double  phi = 2.0 * Math.PI * Xi2;
+        double xs = Math.sin(theta) * Math.cos(phi);
+        double ys = Math.cos(theta);
+        double zs = Math.sin(theta) * Math.sin(phi);
+        Vector4 y = new Vector4(normal);
+        Vector4 h = new Vector4(y);
+        if (Math.abs(h.x)<=Math.abs(h.y) && Math.abs(h.x)<=Math.abs(h.z))
+            h.x= 1.0;
+        else if (Math.abs(h.y)<=Math.abs(h.x) && Math.abs(h.y)<=Math.abs(h.z))
+            h.y= 1.0;
+        else
+            h.z= 1.0;
+        Vector4 x = (h.cross(y)).normalize();
+        Vector4 z = (x.cross(y)).normalize();
+        Vector4 direction = x.scalarMult(xs).add(y.scalarMult(ys)).add(z.scalarMult(zs));
+        direction.w = 0;
+
+        if (direction.dot(normal) < 0) {
+        		direction.scalarMult(-1);
+        }
+        return direction.normalize();
+    }
+
 }
